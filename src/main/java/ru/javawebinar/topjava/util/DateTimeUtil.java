bc6ca24ec2848f2413cb7d88898c33c8bfa9d5f2
Alexandr Nazarov <foxkip@gmail.com>
2020-02-14 12:24:51 +0300

    rename util class

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -6,4 +6,6 @@ public class TimeUtil {
     public static boolean isBetweenInclusive(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
+
+
 }
