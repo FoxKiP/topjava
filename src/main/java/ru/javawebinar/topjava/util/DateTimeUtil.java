1cbe10551966d433deacfbf18cf1a156953f7a5b
Alexandr Nazarov <foxkip@gmail.com>
2020-02-19 21:07:50 +0300

    rename util class

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,15 +1,20 @@
 package ru.javawebinar.topjava.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
+import java.util.Date;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private static final DateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
 
-    public static boolean isBetweenInclusive(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+    public static <T extends Comparable> boolean isBetweenInclusive(T dateOrTime, T start, T end) {
+        return dateOrTime.compareTo(start) >= 0 && dateOrTime.compareTo(end) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
